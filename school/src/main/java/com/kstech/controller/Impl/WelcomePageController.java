029e79742c16d16077b81a89c242fa22625b6160
rite2rohit <rite2rohitjain@gmail.com>
2019-12-11 11:34:40 +0530

    Adding landing page in project

---
+++ school/src/main/java/com/kstech/controller/Impl/WelcomePageController.java
@@ -0,0 +1,14 @@
+package com.kstech.controller.Impl;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class WelcomePageController {
+
+	@RequestMapping("/")
+	public ModelAndView index() {
+		return new ModelAndView("view/index");
+	}
+}
